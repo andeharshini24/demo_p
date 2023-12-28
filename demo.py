405018c1a2ee58bb126d6a470e85f69a5939affc
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2023-12-28 15:27:20 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Set page title
+st.set_page_config(
+    page_title="heloooooooo")
+
+st.markdown("## Heading:")
+    st.write("- hello")
+    st.write("- Cats")
